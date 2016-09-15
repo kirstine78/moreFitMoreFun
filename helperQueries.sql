2bdf2b34b728d7fb8a8ac90b94a2bd0bdb5413cc
kirsti <kirstine78@gmail.com>
2016-09-15 11:43:33 +1000

    Working query for get runs

--- helperQueries.sql
+++ helperQueries.sql
@@ -1,8 +1,14 @@
--- get all runs with route for specific customer
+-- get all runs for specific customer
 SELECT *
 FROM tblrun run, tblRoute route
-WHERE run.fldRunRouteId = route.fldRouteId AND route.fldRouteCustomerId = 1
+WHERE (run.fldRunRouteId = route.fldRouteId AND route.fldRouteCustomerId = 1) 
+OR (run.fldRunCustomerId = 1 AND run.fldRunRouteId is null AND route.fldRouteCustomerId is null)
 
+
+SELECT *
+FROM tblrun run, tblRoute route
+WHERE (run.fldRunRouteId = route.fldRouteId AND route.fldRouteCustomerId = 2) 
+OR (run.fldRunCustomerId = 2 AND run.fldRunRouteId is null AND route.fldRouteCustomerId = 2)
 
 
 SELECT *
@@ -16,6 +22,11 @@ FROM  tblrun
 WHERE fldRunCustomerId = 1 AND fldRunRouteId is null
 
 
+SELECT *
+FROM tblrun run, tblRoute route
+WHERE run.fldRunRouteId = route.fldRouteId AND route.fldRouteCustomerId = 1
+
+
 
 
 SELECT (SELECT *
@@ -23,3 +34,28 @@ FROM tblrun run, tblRoute route
 WHERE run.fldRunRouteId = route.fldRouteId AND route.fldRouteCustomerId = 1)(SELECT *
 FROM  tblrun
 WHERE fldRunCustomerId = 1 AND fldRunRouteId is null)
+
+
+SELECT *
+FROM tblrun run, tblRoute route
+WHERE (run.fldRunRouteId = route.fldRouteId AND route.fldRouteCustomerId = 1) 
+
+union
+
+SELECT *
+FROM  tblrun
+WHERE fldRunCustomerId = 1 AND fldRunRouteId is null
+
+-- ------------------
+
+SELECT run.fldRunId, run.fldDate, route.fldRouteName, route.fldRouteDistance as fldDistance, run.fldSeconds, run.fldFeeling, run.fldRunCustomerId as fldCustomerId, run.fldRunRouteId as fldRouteId
+FROM tblrun run, tblRoute route
+WHERE (run.fldRunRouteId = route.fldRouteId AND route.fldRouteCustomerId = 1) 
+
+union
+
+SELECT fldRunId, fldDate, fldBlankRouteName as fldRouteName, fldDistance, fldSeconds, fldFeeling, fldRunCustomerId as fldCustomerId, fldRunRouteId as fldRouteId
+FROM tblrun
+WHERE fldRunCustomerId = 1 AND fldRunRouteId is null
+
+
